126381720b7b73d932dfc87a4d554a2643504583
Huni\Hunor <ilyeshunor95@gmail.com>
2017-12-03 14:58:32 +0200

    IlyesHunor
    adv

---
+++ app/src/main/java/com/ilyes/sover/sapientiasoverilyes/Advertisment.java
@@ -0,0 +1,71 @@
+package com.ilyes.sover.sapientiasoverilyes;
+
+/**
+ * Created by Hunor on 12/2/2017.
+ */
+
+public class Advertisment
+{
+    String advertismentId;
+    String advertiserId;
+    String title;
+    String description;
+    String location;
+
+    public Advertisment(String advertismentId, String advertiserId, String title, String description, String location)
+    {
+        this.advertismentId = advertismentId;
+        this.advertiserId   = advertiserId;
+        this.title          = title;
+        this.description    = description;
+        this.location       = location;
+    }
+
+    public String getAdvertismentId()
+    {
+        return advertismentId;
+    }
+
+    public void setAdvertismentId(String advertismentId)
+    {
+        this.advertismentId = advertismentId;
+    }
+
+    public String getAdvertiserId() {
+        return advertiserId;
+    }
+
+    public void setAdvertiserId(String advertiserId) {
+        this.advertiserId = advertiserId;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public String getLocation()
+    {
+        return location;
+    }
+
+    public void setLocation(String location)
+    {
+        this.location = location;
+    }
+}
